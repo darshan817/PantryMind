b6c7e41d6df968b9142f006759301e3d4c3d4372
AkshitaInnogent <akshita.jaiswal@innogent.in>
2025-11-19 15:22:06 +0530

    comment

--- backend/src/main/java/com/innogent/pantry_mind/PantryApplication.java
+++ backend/src/main/java/com/innogent/pantry_mind/PantryApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @Entity
 @SpringBootApplication
 public class PantryApplication {
-
+//
 	public static void main(String[] args) {
 		SpringApplication.run(PantryApplication.class, args);
 	}
