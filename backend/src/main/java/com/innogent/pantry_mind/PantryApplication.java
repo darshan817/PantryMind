d463a9518f99cb091392da0d6a07e46caec484fc
AkshitaInnogent <akshita.jaiswal@innogent.in>
2025-11-19 15:14:13 +0530

    jj

--- backend/src/main/java/com/innogent/pantry_mind/PantryApplication.java
+++ backend/src/main/java/com/innogent/pantry_mind/PantryApplication.java
@@ -3,6 +3,7 @@ package com.innogent.pantry_mind;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Entity
 @SpringBootApplication
 public class PantryApplication {
 
